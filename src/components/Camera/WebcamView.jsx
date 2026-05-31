afa89826a6bd129b64b5bff75e50d15e3d3664c4
Julian Diwandara <juliandara17@gmail.com>
2026-05-31 21:25:13 +0700

    update button reverse kamera

--- src/components/Camera/WebcamView.jsx
+++ src/components/Camera/WebcamView.jsx
@@ -1,17 +1,41 @@
 import Webcam from "react-webcam";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
-export default function WebcamView({ webcamRef }) {
+export default function WebcamView() {
+  const [facingMode, setFacingMode] = useState("user");
+
   return (
-    <Webcam
-      ref={webcamRef}
-      audio={false}
-      screenshotFormat="image/png"
-      className="
-      rounded-3xl
-      shadow-2xl
-      w-full
-      lg:max-w-xl md:max-w-lg max-w-base"
-    />
+    <div>
+      <Webcam
+        audio={false}
+        screenshotFormat="image/png"
+        videoConstraints={{
+          facingMode,
+        }}
+      />
+
+      <button
+        onClick={() =>
+          setFacingMode((prev) => (prev === "user" ? "environment" : "user"))
+        }
+        className="absolute lg:bottom-30 md:bottom-30 bottom-45 m-6 p-4 bg-pink-600 rounded-xl text-white hover:bg-pink-700 transition-colors"
+        aria-label="Ganti Kamera"
+      >
+        <svg
+          xmlns="http://w3.org"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={2}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+          />
+        </svg>
+      </button>
+    </div>
   );
 }
